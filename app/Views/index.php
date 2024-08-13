a3463d5edf4ef4dccf3124b1d6e0c3f0ba09224b
Nabila Alawiyah <nabilaalawiyah.25@gmail.com>
2024-08-13 12:39:48 +0700

    belajar ci4 pert 1

---
+++ app/Views/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Web Paling Keren Sedunia</title>
+</head>
+
+<body>
+    <p>keren banget yang bikin</p>
+    <img src="<?= base_url('assets/keren.jpg') ?>" alt="gg">
+</body>
+
+</html>
